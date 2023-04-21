3859257944422f2f64302b3042ab4678819b069f
Bharath Kumar Chowdary Gundala <bharathkumarchowdary006@gmail.com>
2023-04-21 23:41:09 +0530

    adds balance sheet link to header

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Charts from 'components/Charts/Charts';
 import Expenses from 'components/Expenses/Expenses';
 import Header from "components/Header/Header";
-import { Context } from 'provider/Provider';
+import {Context} from 'provider/Provider';
 
 const RouterComponent = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<><Header /><Charts /></>} />
-        <Route path="/expenses" element={<><Header /><Expenses Context={Context} /></>} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<><Header/><Charts/></>}/>
+                <Route path="/expenses" element={<><Header/><Expenses Context={Context}/></>}/>
+                <Route path="/balance_sheet" element={<><Header/><Expenses Context={Context}/></>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default RouterComponent;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,23 @@
-import { AppBar, Toolbar } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { HeaderButton } from './styled';
+import {AppBar, Toolbar} from '@mui/material';
+import {Link} from 'react-router-dom';
+import {HeaderButton} from './styled';
 
 const Header = () => {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <HeaderButton color="inherit" component={Link} to="/">
-          Home
-        </HeaderButton>
-        <HeaderButton color="inherit" component={Link} to="/expenses">
-          Expenses
-        </HeaderButton>
-      </Toolbar>
-    </AppBar>
-  );
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <HeaderButton color="inherit" component={Link} to="/">
+                    Home
+                </HeaderButton>
+                <HeaderButton color="inherit" component={Link} to="/expenses">
+                    Expenses
+                </HeaderButton>
+                <HeaderButton color="inherit" component={Link} to="/balance_sheet">
+                    Balance Sheet
+                </HeaderButton>
+            </Toolbar>
+        </AppBar>
+    );
 };
 
 export default Header;
